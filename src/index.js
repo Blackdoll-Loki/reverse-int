3be33f9360e4df88ac8b3584158d4168d9b844b8
Blackdoll-Loki <77502207+Blackdoll-Loki@users.noreply.github.com>
2021-08-19 16:41:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let positive = Math.abs(n);
+  let str = positive.toString();
+  let arr = str.split("");
+  let reversedStr = arr.reverse().join("");
+  return +reversedStr;
 }
